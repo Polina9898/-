940b3875cd7a31a4dbf95ac06ebae51ab1c03993
Polina <Polin_a_@mail.ru>
2023-01-13 16:42:49 +0100

    solution

--- task/Program.cs
+++ task/Program.cs
@@ -8,12 +8,28 @@ Console.WriteLine("Введите длину массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 string?[] array = new string?[n];
+int count = 0;
 
 for (int i = 0; i < array.Length; i++) {
     Console.Write("Введите элемент массива: ");
     array[i] = Console.ReadLine();
+
+    if (array[i]?.Length <= 3) {
+        count = count + 1;
+    }
 }
 
-Console.WriteLine($"[{string.Join(", ", array)}]");
+int j = 0;
+string?[] array1 = new string?[count];
+
+for (int i = 0; i < array.Length; i++) {
+    if (array[i]?.Length <= 3) {
+        array1[j] = array[i];
+        j = j + 1;
+    }
+}
+
+Console.WriteLine($"[{string.Join(", ", array1)}]");
+
 
 
